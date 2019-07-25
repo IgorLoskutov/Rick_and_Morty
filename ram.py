05fb1fcea996d60afe3f736b21902f2853c44379
sieger <sieger_p@mail.ru>
2019-07-25 11:15:19 +0300

    url as command line argument, multiprocessing

--- ram.py
+++ ram.py
@@ -1,19 +1,21 @@
 import asyncio
 import requests
 
-from bs4 import BeautifulSoup
-
 import subprocess
 
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
-from time import time
+from time import time, strftime, localtime
 
 import os
+import sys
+import getopt
 import shutil
 
 from seleniumwire import webdriver
 
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -26,50 +28,69 @@ class EpisodeDownloader:
     save and concatenate to a single file
     """
 
-    def __init__(self, start_url):
+    def __init__(self, start_url, season):
         """ obj instance created in context of driver process """
-        self.driver = webdriver.Firefox()
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("permissions.default.image", 2)
+        # 1 - Allow all images
+        # 2 - Block all images
+        # 3 - Block 3rd party images
+
+        self.driver = webdriver.Firefox(firefox_profile=profile)
+        self.season = season
         self.start_url = start_url
         self.streams_links = self._get_episodes()
-        self.streams_list = None  # delivered by   __get_chunks()
+        self.streams_list = None  # delivered by _get_chunks()
         self._get_chunks()
-        # self.folder = start_url[-8:].replace(':', '').replace('-', '')
-        # if not os.path.exists(self.folder):
-        #     os.mkdir(self.folder)
 
     def _get_episodes(self):
+
         playlists_links = dict()
-        self.driver.get(self.start_url)
+        attempt = 1
+        while attempt <= 4:
+            try:
+                self.driver.get(self.start_url)
+                print(f"getting season{self.season} episodes started attempt {attempt}")
+                # scroll page to see frame in viewport
+                player = self.driver.find_element_by_id('player')
+                self.driver.execute_script("arguments[0].scrollIntoView();", player)
 
-        # scroll page to see iframe in viewport
-        player = self.driver.find_element_by_id('player')
-        self.driver.execute_script("arguments[0].scrollIntoView();", player)
+                # click play_button for each episode to catch link to its playlist
 
-        # click play_button for each episode to catch link to its playlist
-        episodes_buttons = self.driver.find_elements_by_xpath(
-            "//ul[@id='simple-episodes-list-1']/li[@data-season_id='1']"
-        )
-        for episode in episodes_buttons:
-            episode.click()
-            WebDriverWait(self.driver, 30).until(
-                ec.frame_to_be_available_and_switch_to_it('cdn-player'))
-            elem = WebDriverWait(self.driver, 30).until(
-                ec.element_to_be_clickable((By.ID, 'play_button'))
-            )
-            elem.click()
-            self.driver.switch_to.default_content()
-            self.driver.wait_for_request('/index.m3u8', timeout=60)
+                episodes_buttons = self.driver.find_elements_by_xpath(
+                    f"//ul[@id='simple-episodes-list-{self.season}']/li[@data-season_id]"
+                )
+                for episode in episodes_buttons:
+                    episode.click()
+                    WebDriverWait(self.driver, 30).until(
+                        ec.frame_to_be_available_and_switch_to_it('cdn-player'))
+                    elem = WebDriverWait(self.driver, 30).until(
+                        ec.element_to_be_clickable((By.ID, 'play_button'))
+                    )
+                    elem.click()
+                    self.driver.switch_to.default_content()
+                    self.driver.wait_for_request('/index.m3u8', timeout=10)
 
-            # seleniumwire .request tracks all requests done by browser
-            for request in self.driver.requests:
-                if request.response and "/index.m3u8" in request.path:
-                    playlists_links[episode.text] = \
-                        request.response.body.decode('utf-8').split('\n')[4]
+                    # seleniumwire .request tracks all requests done by browser
+                    for request in self.driver.requests:
+                        if request.response and "/index.m3u8" in request.path:
+                            playlists_links[episode.text] = \
+                                request.response.body.decode('utf-8').split('\n')[4]
 
-            self.driver.switch_to.default_content()
-            del self.driver.requests  # clean the list of requests tracked by seleniumwire
-
+                self.driver.switch_to.default_content()
+                del self.driver.requests  # clean the list of requests tracked by seleniumwire
+                print(f"getting season{self.season} episodes complete")
+                break
+            except TimeoutException:
+                if attempt > 4:
+                    print(f"loading season {self.season} episodes list failed after 4 attempts ")
+                    break
+                attempt += 1
+                self.driver.switch_to.default_content()
+                del self.driver.requests  # clean the list of requests tracked by seleniumwire
+                continue
         self.driver.close()
+
         return playlists_links
 
     def _get_chunks(self):
@@ -109,10 +130,10 @@ class EpisodeDownloader:
         return lst[7::2]
 
     @staticmethod
-    def _chunk_url(plailist_url, chunks_list):
+    def _chunk_url(playlist_url, chunks_list):
         """ return the list of chunk_urls for files from playlist """
 
-        location = plailist_url.split('/')[:-1]
+        location = playlist_url.split('/')[:-1]
         location_url = '/'.join(location)
 
         chunks_url_list = list(
@@ -126,7 +147,7 @@ class EpisodeDownloader:
     @staticmethod
     def req_get(url):
         """ request get + print(url) to display that requests goes async-ly
-         just for visualization than program not hanged
+         just for visualization that program not hanged
          """
 
         rqst = requests.get(url)
@@ -165,13 +186,13 @@ class EpisodeDownloader:
                                 self.streams_list[episode]
                                 )
             )
-
             loop = asyncio.get_event_loop()
             future = asyncio.ensure_future(self.load_episode(ep_tuple))
             loop.run_until_complete(future)
 
     def make_single_file(self):
-        """ glue all chunks of the stream into a single video file """
+        """ glue all chunks of the stream into a single video file
+        ffmpeg required for processing vide ofiles"""
 
         for episode in self.streams_list:
             chunk_list = list(map(
@@ -179,6 +200,7 @@ class EpisodeDownloader:
                 self.streams_list[episode])
             )[:]
             command_string = 'concat:' + '|'.join(chunk_list)
+
             subprocess.call(['ffmpeg',
                              '-i',
                              command_string,
@@ -191,17 +213,82 @@ class EpisodeDownloader:
 
 
 class SeasonDownloader:
-    pass
+    """Download all seasons
+    Check number of seasons - Open target page, scroll to player window and find seasons list.
+
+    """
+    def __init__(self, start_url):
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("permissions.default.image", 2)
+        # 1 - Allow all images
+        # 2 - Block all images
+        # 3 - Block 3rd party images
+        self.driver = webdriver.Firefox(firefox_profile=profile)
+        self.start_url = start_url
+        self.seasons_urls = self._get_seasons()
+
+    def _get_seasons(self):
+        self.driver.get(self.start_url)
+        player = self.driver.find_element_by_id('player')
+        self.driver.execute_script("arguments[0].scrollIntoView();", player)
+        seasons_buttons = self.driver.find_elements_by_xpath(
+            "//ul[@id='simple-seasons-tabs']/li[@data-tab_id]"
+        )
+
+        seasons = [_.get_attribute('data-tab_id') for
+                   _ in seasons_buttons]
+        s_url = self.start_url + '#t:{}-s:{}-e:1'
+
+        seasons_urls = {se: s_url.format(self._get_trans(), se) for se in seasons}
+
+        self.driver.close()
+        return seasons_urls
+
+    def _get_trans(self):
+        return self.driver.find_elements_by_xpath(
+            "//ul[@id='translators-list']/li[@*]"
+        )[0].get_attribute('data-translator_id')
 
 
 if __name__ == "__main__":
     start = time()
-    season_1_url = "https://rezka.ag/cartoons/comedy/2136-rik-i-morti-2013.html#t:66-s:1-e:1"
+    season_1_url = ''
+    try:
+        if not sys.argv[1:]:
+            raise getopt.GetoptError('missig url')
+        opts, args = getopt.getopt(sys.argv[1:], "hu:", ["url="])
+    except getopt.GetoptError:
+        print('ram.py -u <url>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('ram.py -u <url>')
+            sys.exit()
+        elif opt in ("-u", "--url"):
+            season_1_url = arg
 
-    season_1 = EpisodeDownloader(season_1_url)
+    mov = SeasonDownloader(season_1_url)
+    print(mov.seasons_urls)
 
-    season_1.download_episodes()
-    season_1.make_single_file()
+    def multi(url_season: tuple):
+        season_folder = f"season{url_season[0]} - {strftime('%H:%M:%S', localtime())}"
+        os.mkdir(season_folder)
+        os.chdir(season_folder)
+        season = EpisodeDownloader(url_season[1], url_season[0])
+
+        season.download_episodes()
+        try:
+            season.make_single_file()
+        except FileNotFoundError:
+            print("This program require ffmpeg installed. Please install and try again")
+            return
+        finally:
+            os.chdir("../")
+
+    with ProcessPoolExecutor() as executor:
+        executor.map(multi, mov.seasons_urls.items())
 
     end = time()
     print(end - start)
+    
+
